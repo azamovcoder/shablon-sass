c4f2f102f1ea7a597d3c2b5de27aab392c23eec3
azamovcoder <azamovcoder@gmail.com>
2024-03-07 12:07:36 +0300

    shablon|Scss

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector(".header__hamburger");
+let navBar =document.querySelector(".header__list");
+
+
+menu.addEventListener("click", function ()
+{
+    navBar.classList.toggle("header__show-list");
+});
